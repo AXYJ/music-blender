969a6375791eead55ec54554a4704afc58964a47
Alex Xiao <alex.xiao.26215@stu.he-ferrer.eu>
2026-08-25 19:02:35 +0200

    Ajout redirection de music-blender à museek

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,6 +13,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "music-blender.xiao-web.com",
+          },
+        ],
+        destination: "https://museek.xiao-web.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
